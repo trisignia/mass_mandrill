e9fd83e1e78d7233ed3a16edbfb14957d83fba79
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-01-08 20:49:09 +0100

    Remove Config

--- lib/mass_mandrill.rb
+++ lib/mass_mandrill.rb
@@ -1,13 +1,2 @@
-require 'mass_mandrill/config'
 require 'mass_mandrill/mandrill_mailer'
 require 'mass_mandrill/mandrill_mail'
-
-module MassMandrill
-  def self.config
-    if block_given?
-      yield Config.instance
-    else
-      Config.instance
-    end
-  end
-end
--- lib/mass_mandrill/config.rb
+++
@@ -1,9 +0,0 @@
-require 'singleton'
-
-module MassMandrill
-  class Config
-    include Singleton
-    #api_key is now read automatically from ENV in the official mandrill-api gem
-    #attr_accessor :api_key
-  end
-end
